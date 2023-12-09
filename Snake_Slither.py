22e1a7dae38dfe722db56aa459fac38b5e95fe49
Ricky Ega Pramana <101786804+rickyegaa@users.noreply.github.com>
2023-12-09 07:30:51 +0700

    feature: gameOver (#2)

--- Snake_Slither.py
+++ Snake_Slither.py
@@ -2,22 +2,24 @@ from OpenGL.GLUT import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from random import randrange
+from time import sleep
 
 # Inisialisasi Variabel
 window = 0
-width, height = 500, 500
+width, height = 600, 600
 field_width, field_height = 50, 50
 
-snake = [(40, 40), (39, 40), (38, 40)]
+snake = [(25, 7), (25, 6), (25, 5)]
 level = 1
-snake_dir = (1, 0)
+snake_dir = (0, 1)
 interval = 300
 food = [(25, 25)]
-state = 'right'
-current_direction = (1, 0)
+state = 'up'
+current_direction = (0, 1)
 score = 0
 collision = False
 r, g, b = 1, 1, 1
+high_score = 0
 
 # Fungsi Untuk Mengatur Tampilan 2D Pada Jendela Permainan
 def custom_2D_gameWindow(internal_width, internal_height):
@@ -60,9 +62,33 @@ def board():
 def score_display():
     glColor3f(0, 1, 0)
     glRasterPos2f(1, 47)
-    score_str = "Score : {} | Level : {}".format(score, level)
+    score_str = "Score : {}  |  Level : {}".format(score, level)
     for char in score_str:
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
+
+# Fungsi Untuk Menampilkan Layar Game Over
+def game_over_display():
+    global high_score
+    glColor3f(1, 0, 0)
+    glRasterPos2f(field_width // 2 - 5, field_height // 2 + 2)
+    game_over_str = "Game Over"
+    for char in game_over_str:
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ord(char))
+
+    # Tampilkan skor dan skor tertinggi
+    glColor3f(0, 1, 0)
+    glRasterPos2f(field_width // 2 - 6, field_height // 3 + 6)
+    final_score_str = f"High Score : {score}"
+    for char in final_score_str:
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ord(char))
+
+    # Perbarui skor tertinggi jika skor saat ini lebih tinggi
+    if score > high_score:
+        high_score = score
+
+    glutSwapBuffers()
+    sleep(5)
+    reset_game()
 
 # Fungsi Untuk Menggambar Persegi Panjang Dengan Sudut Kiri Atas di (x, y), lebar Width, dan Tinggi Height.
 def draw_rect(x, y, width, height):
@@ -75,13 +101,26 @@ def draw_rect(x, y, width, height):
 
 # Fungsi Untuk Menggambar Seluruh Tubuh Ular
 def draw_snake():
+    global r, g, b
+    head = snake[0]
+
+    # Cek apakah kepala ular menabrak dinding atau tubuhnya sendiri
+    if (
+        head[0] == 2 or head[0] == 47 or
+        head[1] == 2 or head[1] == 45 or
+        head in snake[1:]
+    ):
+        r, g, b = 1, 0, 0  
+    else:
+        r, g, b = 1, 1, 1 
+
     glColor3f(r, g, b)
     for x, y in snake:
         draw_rect(x, y, 1, 1)
 
 # Fungsi untuk Menggambar Makanan Ular
 def draw_food():
-    glColor3f(0, 1, 0)
+    glColor3f(1, 1, 0)
     draw_rect(food[0][0], food[0][1], 1, 1)
 
 # Fungsi Untuk Menggambar Semua Elemen Tampilan Permainan Seperti Makanan, Ular, Skor, dan Batasan Bidang.
@@ -104,19 +143,15 @@ def move_snake():
     new_head = (snake[0][0] + snake_dir[0], snake[0][1] + snake_dir[1])
 
     # Cek tabrakan dengan dinding
-    if (
-        new_head[0] < 2
-        or new_head[0] >= field_width - 2
-        or new_head[1] < 3
-        or new_head[1] >= field_height - 2
-    ):
+    if snake[0][0] == 2 or snake[0][0] == 47 or snake[0][1] == 2 or snake[0][1] == 45:
         collision = True
-        return
 
     # Cek tabrakan dengan tubuh ular
-    if new_head in snake:
-        collision = True
-        return
+    head = snake[0]
+    for i in range(1, len(snake)):
+        body = snake[i]
+        if head[0] == body[0] and head[1] == body[1]:
+            collision = True
 
     # Cek apakah ular memakan makanan
     if new_head == food[0]:
@@ -128,27 +163,35 @@ def move_snake():
             interval -= 50  # Atur interval agar permainan menjadi lebih cepat
 
         # Posisi makanan baru harus di dalam dinding
-        while True:
-            new_food = (randrange(3, field_width - 2), randrange(3, field_height - 2))
-            if new_food not in snake:
-                food = [new_food]
-                break
-    else:
-        # Jika tidak, perbarui tubuh ular (hapus ekor)
-        snake.pop()
+        food = [(randrange(3, 47), randrange(3, 45))]
 
     # Tambahkan kepala baru pada posisi depan
     snake.insert(0, new_head)
 
+    # Hapus ekor ular jika panjang ular lebih dari skor
+    if len(snake) > score + 2:
+        snake.pop()
+
 
 def reset_game():
-    global snake, snake_dir, food, score, collision, r, g, b, level, interval
-    snake = [(40, 40), (39, 40), (38, 40)]
-    snake_dir = (1, 0)
+    global snake, level, snake_dir, interval, food, state, current_direction, score, collision, r, g, b
+    snake = [(25, 7), (25, 6), (25, 5)]
+    level = 1
+    snake_dir = (0, 1)
     interval = 300
     food = [(25, 25)]
+    state = 'up'
+    current_direction = (0, 1)
+    score = 0
     collision = False
+    r, g, b = 1, 1, 1
 
+# Fungsi untuk memutuskan apakah harus menampilkan layar game over pada tampilan permainan 
+def draw_game_over_display():
+    if collision:
+        game_over_display()
+    else:
+        draw_display()
 
 # Fungsi callback untuk mengatur logika permainan
 def game_logic(value):
@@ -160,9 +203,9 @@ def game_logic(value):
     glutTimerFunc(interval, game_logic, 0)
     glutPostRedisplay()
 
-    # Reset collision status when the snake collides
+    # Cek kondisi Game Over
     if collision:
-        reset_game()
+        draw_game_over_display()
 
 # Fungsi callback untuk menanggapi input kursor panah
 def special_key_input(key, x, y):
